e6f0addf88415c46c870f29f11d3f5c0398ec375
jeon-seongjae <tjdwoek62@google.com>
2022-01-25 16:32:17 +0900

    standardSetting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+      // production 환경일 때는 configModule이 환경변수 파일을 무시한다
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
